cb4af9861c320acb1323d48587dedbf59992edf9
Linknpay <gk@linknpay.com>
2024-11-28 15:30:25 +0100

    v1.1.0: sync fork

--- dist/esm/web.d.ts
+++ dist/esm/web.d.ts
@@ -1,17 +1,30 @@
 import { WebPlugin } from "@capacitor/core";
-import type { SocialLoginPlugin, InitializeOptions, LoginOptions, LoginResult, AuthorizationCode } from "./definitions";
+import type { SocialLoginPlugin, InitializeOptions, LoginOptions, LoginResult, AuthorizationCode, isLoggedInOptions, AuthorizationCodeOptions } from "./definitions";
 export declare class SocialLoginWeb extends WebPlugin implements SocialLoginPlugin {
-    getAuthorizationCode(): Promise<AuthorizationCode>;
-    echo(options: {
-        value: string;
-    }): Promise<{
-        value: string;
-    }>;
+    private googleClientId;
+    private appleClientId;
+    private googleScriptLoaded;
+    private appleScriptLoaded;
+    private appleScriptUrl;
+    private facebookAppId;
+    private facebookScriptLoaded;
     initialize(options: InitializeOptions): Promise<void>;
     login(options: LoginOptions): Promise<LoginResult>;
-    logout(options: any): Promise<void>;
-    refresh(options: LoginOptions): Promise<void>;
-    isLoggedIn(options: any): Promise<{
+    logout(options: {
+        provider: "apple" | "google" | "facebook";
+    }): Promise<void>;
+    isLoggedIn(options: isLoggedInOptions): Promise<{
         isLoggedIn: boolean;
     }>;
+    getAuthorizationCode(options: AuthorizationCodeOptions): Promise<AuthorizationCode>;
+    refresh(options: LoginOptions): Promise<void>;
+    private loginWithGoogle;
+    private parseJwt;
+    private loadGoogleScript;
+    private loginWithApple;
+    private loadAppleScript;
+    private getGoogleUser;
+    private loadFacebookScript;
+    private loginWithFacebook;
+    private fallbackToTraditionalOAuth;
 }
